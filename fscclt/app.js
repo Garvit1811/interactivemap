d310cf22800d0608ea5cb183de2eacd63b78ff1c
Garvit1811 <chessician1811@gmail.com>
2026-03-19 13:57:58 -0700

    Update community tour figures and trim duplicate gallery images

--- fscclt/app.js
+++ fscclt/app.js
@@ -7,12 +7,12 @@ document.addEventListener('DOMContentLoaded', () => {
         onMapInit: (map, L) => {
             const busStopIcon = L.divIcon({
                 className: 'poi-marker-wrapper',
-                html: '<div class="poi-marker" title="4th &amp; Heather Bus Stop">B</div>',
+                html: '<div class="poi-marker" title="Heather Square on Moberly Road">B</div>',
                 iconSize: [24, 24],
                 iconAnchor: [12, 12]
             });
             L.marker([49.2668, -123.1185], { icon: busStopIcon })
-                .bindTooltip('Tour Start: 4th & Heather Bus Stop', {
+                .bindTooltip('Tour Start: 6th at Heather', {
                     direction: 'top', offset: [0, -16], className: 'marker-tooltip'
                 })
                 .addTo(map);
